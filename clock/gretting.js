2e1f68fc0fa2b3085327e692cc42bfaf510f5c54
codms <chany99_88@naver.com>
2020-08-26 03:05:21 +0900

    clock

--- clock/gretting.js
+++ clock/gretting.js
@@ -5,16 +5,33 @@ const form = document.querySelector(".js-form"),
 const USER_LS = "currentUser",
     SHOWING_CN = "showing";
 
+function saveName(text) {
+    localStorage.setItem(USER_LS, text);
+}
+
+function handleSubmit(event){
+    event.preventDefault();
+    const currentValue = input.value;
+    paintGreeting(currentValue);
+    saveName(currentValue);
+}
+
+function askForName() {
+    form.classList.add(SHOWING_CN);
+    form.addEventListener("submit", handleSubmit);
+}
+
+
 function paintGreeting(text){
     form.classList.remove(SHOWING_CN); //색 보이려면 폼을 숨겨야함?
     greeting.classList.add(SHOWING_CN);
-    greeting.innerText = `Hello${text}`;
+    greeting.innerText = `Hello ${text}`;
 }
 
 function loadName(){
     const currentUser = localStorage.getItem(USER_LS);
-    if(currentUser === null){
-        // she is not
+    if(currentUser === null) {
+        askForName();
     } else {
         paintGreeting(currentUser);// she is
     }
