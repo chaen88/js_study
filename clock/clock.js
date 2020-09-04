23ed725cf4714952d6eb01b923b9f5a62ecce44e
codms <chany99_88@naver.com>
2020-09-04 18:53:42 +0900

    painting

--- clock/clock.js
+++ clock/clock.js
@@ -3,11 +3,15 @@ const clockContainer = document.querySelector(".js-clock"),
 
 function getTime(){
     const date = new Date();
+    const year = date.getFullYear();
+    const months = date.getMonths();
     const minutes = date.getMinutes();
     const hours = date.getHours();
     const seconds = date.getSeconds();
-    clockTitle.innerText=`${hours < 10 ? `0${hours}` : hours}:${
-     minutes  < 10 ? `0${minutes}` : minutes
+    const millisecond = date.getMilliseconds();
+    clockTitle.innerText=`${months < 10 ? `0${months}` : months} :${
+    hours < 10 ? `0${hours}` : hours}:${
+    minutes  < 10 ? `0${minutes}` : minutes
     }:${seconds < 10 ? `0${seconds}` : seconds}`;
     
 } //안에 객체 넣기//
